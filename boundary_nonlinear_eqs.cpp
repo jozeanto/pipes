e89f12f974987276dae4e2b5bb8af89a43a955b4
jozeanto <jozeanto@gmail.com>
2017-08-31 12:18:17 +0200

    python and cpp

--- boundary_nonlinear_eqs.cpp
+++ boundary_nonlinear_eqs.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 
-#define Scalar double
+#define Scalar float
 #define pi 3.141592653589793238L
 
 
 Scalar hu=0.5;
 Scalar d=0.6;
 Scalar Q=0.1;
-Scalar g=9.81;
+ Scalar g=9.81;
 
 
 /*
@@ -29,6 +29,28 @@ Scalar dff(Scalar t){
                     /( pow(16*sin(t/2),2) ));
 }
 
+class Ff{
+  public:
+    Scalar hu,d;
+    Ff (Scalar x, Scalar y) : hu( x ),d(y) {}
+    Scalar operator()(Scalar t){
+        return  hu/d-(1-cos(t/2))/2 -(t-sin(t))/(16*sin(t/2));
+    }
+};
+
+
+class DFf{
+  public:
+    Scalar hu,d;
+    DFf (Scalar x, Scalar y) : hu( x ),d(y) {}
+    Scalar operator()(Scalar t){
+        return  (-1./2*sin(t/2)*1./2
+                    -( (1-cos(t))*16*sin(t/2) 
+                    -(t-sin(t))*16*cos(t/2)*1./2 )  
+                    /( pow(16*sin(t/2),2) ));
+    }
+};
+
 /*
  * Flujo de salida subcritico en lamina libre
  * --------------------------------------------
@@ -40,6 +62,26 @@ Scalar fg(Scalar t){
 Scalar dfg(Scalar t){
   return  -(3.*pow(t-sin(t),2)*(1-cos(t))*sin(t/2)-pow(t-sin(t),3)*cos(t/2)*1./2 )/pow(sin(t/2),2);
 }
+
+
+class Fg{
+  public:
+    Scalar hu,d;
+    Fg (Scalar x, Scalar y) : hu( x ),d(y) {}
+    Scalar operator()(Scalar t){
+        return  512.*Q/(g*pow(d,5))-pow(t-sin(t),3)/sin(t/2.);
+    }
+};
+
+
+class DFg{
+  public:
+    Scalar hu,d;
+    DFg (Scalar x, Scalar y) : hu( x ),d(y) {}
+    Scalar operator()(Scalar t){
+        return  -(3.*pow(t-sin(t),2)*(1-cos(t))*sin(t/2)-pow(t-sin(t),3)*cos(t/2)*1./2 )/pow(sin(t/2),2);
+    }
+};
 
 
 /*
@@ -54,7 +96,34 @@ Scalar dfh(Scalar t){
   return  -(3.*pow(t-sin(t),2)*(1-cos(t))*sin(t/2)-pow(t-sin(t),3)*cos(t/2)*1./2 )/pow(sin(t/2),2);
 }
 
-Scalar newton ( Scalar (&f)(Scalar), Scalar (&df)(Scalar) ,Scalar x,int n, Scalar eps){
+class Fh{
+  public:
+    Scalar hu,d;
+    Fh (Scalar x, Scalar y) : hu( x ),d(y) {}
+    Scalar operator()(Scalar t){
+        return  512.*Q/(g*pow(d,5))-pow(t-sin(t),3)/sin(t/2.);
+    }
+};
+
+
+class DFh{
+  public:
+    Scalar hu,d;
+    DFh (Scalar x, Scalar y) : hu( x ),d(y) {}
+    Scalar operator()(Scalar t){
+        return  -(3.*pow(t-sin(t),2)*(1-cos(t))*sin(t/2)-pow(t-sin(t),3)*cos(t/2)*1./2 )/pow(sin(t/2),2);
+    }
+};
+
+
+
+/*
+ * Generic Newton method
+ * --------------------------------------------
+ */
+
+template <typename FuncType,typename DFuncType>
+Scalar newton ( FuncType& f, DFuncType& df ,Scalar x,int n, Scalar eps){
   int it=0;
   while ( (fabs(f(x)) > eps) && (it<n)){
     x=x-f(x)/df(x);
@@ -63,6 +132,7 @@ Scalar newton ( Scalar (&f)(Scalar), Scalar (&df)(Scalar) ,Scalar x,int n, Scala
   }
   return x;
 }
+
 
 
 
@@ -75,6 +145,12 @@ int main(){
   x=2.*pi*0.9999;
   cout<<"newton value "<<newton(fg,dfg,x,40,1e-12)<<endl;
 //   cout<<newton(ff,dff,0.001,20,1e-6)<<endl;
+  
+  Ff fff(hu,d);
+//   fff.hu=hu;
+//   fff.d=d;
+  x=2.*pi*0.9999;
+  cout<<"newton value "<<newton(fff,dff,x,40,1e-12)<<endl;
 
   
 }
